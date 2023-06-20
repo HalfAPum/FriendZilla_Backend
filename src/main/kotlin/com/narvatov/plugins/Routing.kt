51a3a8a1a157c90046319159398198a3105f0568
Oleksandr Narvatov <hipi96222@gmail.com>
2023-06-20 17:43:07 +0300

    Add postgresql database to project

--- src/main/kotlin/com/narvatov/plugins/Routing.kt
+++ src/main/kotlin/com/narvatov/plugins/Routing.kt
@@ -3,9 +3,22 @@ package com.narvatov.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 
 fun Application.configureRouting() {
     routing {
+        route("/user") {
+            post("/sign-up") {
+                val body = call.receive<String>()
+                call.respond(body)
+            }
+
+            delete("/delete-account/{id}") {
+                val id = call.parameters["id"]
+                call.respond(id.toString())
+            }
+        }
+
         get("/") {
             call.respondText("Hello World!")
         }
