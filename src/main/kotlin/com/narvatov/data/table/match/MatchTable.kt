23c015ba2bc42a9b940124190264853420167e82
Oleksandr Narvatov <hipi96222@gmail.com>
2023-06-21 09:00:08 +0300

    Add match relation to user table

--- src/main/kotlin/com/narvatov/data/table/match/MatchTable.kt
+++ src/main/kotlin/com/narvatov/data/table/match/MatchTable.kt
@@ -1,12 +1,13 @@
 package com.narvatov.data.table.match
 
 import com.narvatov.data.table.user.UserTable
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
 object MatchTable : Table() {
 
-    val matcherId = varchar("matcher_id", 20).references(UserTable.id)
-    val responderId = varchar("responder_id", 20).references(UserTable.id)
+    val matcherId = varchar("matcher_id", 20).references(UserTable.id, onDelete = ReferenceOption.CASCADE)
+    val responderId = varchar("responder_id", 20).references(UserTable.id, onDelete = ReferenceOption.CASCADE)
 
     override val primaryKey = PrimaryKey(matcherId, responderId)
 
