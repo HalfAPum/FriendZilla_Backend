2d5cc81e16345e506e765cb1ecd61e5de72de5c8
Oleksandr Narvatov <hipi96222@gmail.com>
2023-06-27 17:14:33 +0300

    Fix jdbc url

--- src/main/kotlin/com/narvatov/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/narvatov/repository/DatabaseFactory.kt
@@ -10,28 +10,39 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
+import java.sql.DriverManager
+
 
 object DatabaseFactory {
 
     fun init(config: ApplicationConfig) {
+//        val dbUri = URI("")
+//
+//        val username: String = dbUri.getUserInfo().split(":").get(0)
+//        val password: String = dbUri.getUserInfo().split(":").get(1)
+//        val dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+//
+//        return DriverManager.getConnection(dbUrl, username, password)
+
         Database.connect(hikari(config))
 //        val driverClassName = config.property("storage.driverClassName").getString()
 //        val jdbcURL = config.property("storage.jdbcURL").getString()
 //        Database.connect(jdbcURL, driverClassName, user = "postgres", password = "admin")
 //
-//        transaction {
-//            SchemaUtils.create(UserTable)
-//        }
-//
-//        transaction {
-//            SchemaUtils.create(MatchTable)
-//        }
+        transaction {
+            SchemaUtils.create(UserTable)
+        }
+
+        transaction {
+            SchemaUtils.create(MatchTable)
+        }
     }
 
     fun hikari(appConfig: ApplicationConfig): HikariDataSource {
         val config = HikariConfig().apply {
             driverClassName = "org.postgresql.Driver"
-            jdbcUrl = "jdbc:postgresql://postgres:EC0Z3CfXSG56qvW3ui4d@container?user=postgres&password=EC0Z3CfXSG56qvW3ui4d"
+            jdbcUrl = "jdbc:postgresql://postgres:EC0Z3CfXSG56qvW3ui4d@containers-us-west-176.railway.app:7328/railway?user=postgres&password=EC0Z3CfXSG56qvW3ui4d"
 //            username = "postgres"
 //            password = "EC0Z3CfXSG56qvW3ui4d"
             maximumPoolSize = 3
